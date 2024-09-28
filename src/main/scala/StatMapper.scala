b25264a07551a2352b8fe47e1f9248a9fee92938
Gautham YS <gauthamys@Gauthams-MacBook-Air.local>
2024-09-28 02:10:18 -0500

    remove numbers and \n

--- src/main/scala/StatMapper.scala
+++ src/main/scala/StatMapper.scala
@@ -8,21 +8,30 @@ class StatMapper extends Mapper[LongWritable, Text, Text, LongWritable] {
   private val one = new LongWritable(1)
   private val registry = Encodings.newDefaultEncodingRegistry()
   private val encoding = registry.getEncoding(EncodingType.CL100K_BASE)
+  // private val logger = new Logger()
 
   override def map(key: LongWritable, value: Text, context: Mapper[LongWritable, Text, Text, LongWritable]#Context): Unit = {
-    // Split each line into words
+    // Clean each line - remove punctuation, convert all words to lowercase and map <wordEncoding, 1>
     val strValue = value.toString
-    var cleanedText = strValue.replaceAll("[().,?!:;\"'~`{}><+=^%$/#@*-]", "").toLowerCase
+
+    // logger.info("Cleaning up input text")
+    var cleanedText = strValue.trim.replaceAll("[().,?!:;\"'{}%$/<>+-]", " ").toLowerCase
+    cleanedText = cleanedText.replaceAll("[0-9]", "")
     cleanedText = cleanedText.replace("[", "")
     cleanedText = cleanedText.replace("]", "")
+    cleanedText = cleanedText.replace("\n", "")
+
+    // logger.info("Splitting Tokens")
     val tokens = cleanedText.split("\\s+")
 
+    // logger.info("Creating Byte Pair Encodings")
     tokens.foreach { token =>
       val bpeTokens = encoding.encode(token)
       bpeTokens.toArray.foreach { bpeToken =>
         context.write(new Text(bpeToken.toString), one)
       }
     }
+
   }
 
 }
--- src/main/scala/StatReducer.scala
+++ src/main/scala/StatReducer.scala
@@ -10,27 +10,29 @@ class StatReducer extends Reducer[Text, LongWritable, Text, Text] {
   private val registry = Encodings.newDefaultEncodingRegistry()
   private val encoding = registry.getEncoding(EncodingType.CL100K_BASE)
   private val vocabulary = new mutable.HashSet[String]()
+  
   override def reduce(key: Text, values: java.lang.Iterable[LongWritable], context: Reducer[Text, LongWritable, Text, Text]#Context): Unit = {
+    // sum all occurrences of the word and add the key to the vocabulary set
     var sum = 0L
-    val keys = new IntArrayList()
+    val keys = new IntArrayList()       // array for passing to decode function
     val sumText = new Text()
     val keyText = new Text()
-    val iter = values.iterator()
+    val iter = values.iterator()        // iterate over values
     while (iter.hasNext) {
       sum = sum + iter.next().get()
     }
-    vocabulary += key.toString
+    vocabulary += key.toString          // add the key to the vocabulary set
     keys.add(key.toString.toInt)
     sumText.set(sum.toString)
     keyText.set(encoding.decode(keys))
-
-    val resText = new Text()
+    val resText = new Text()            // output
     resText.set(key.toString + "," + keyText.toString + "," + sum.toString)
-
     context.write(null, resText)
   }
+
   override def cleanup(context: Reducer[Text, LongWritable, Text, Text]#Context): Unit = {
     // After processing all words, output the vocabulary size
     context.write(null, new Text(s"Vocabulary Size,${vocabulary.size}"))
   }
+
 }
