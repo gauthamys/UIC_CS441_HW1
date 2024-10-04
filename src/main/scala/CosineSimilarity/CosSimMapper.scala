e947916d577ad4a593f1374fa6c4dcbce540f681
Gautham YS <gauthamys@Gauthams-MacBook-Air.local>
2024-10-03 22:40:15 -0500

    embedding-shard

--- src/main/scala/CosineSimilarity/CosSimMapper.scala
+++ src/main/scala/CosineSimilarity/CosSimMapper.scala
@@ -5,6 +5,6 @@ import org.apache.hadoop.mapreduce.Mapper
 
 class CosSimMapper extends Mapper[LongWritable, Text, Text, Text]{
   override def map(key: LongWritable, value: Text, context: Mapper[LongWritable, Text, Text, Text]#Context): Unit = {
-
+    
   }
 }
